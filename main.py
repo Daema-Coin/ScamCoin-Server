de84d096013e77ffdcc9a1e0c6038c1a01150c14
이하성 <tedsoftj1123@gmail.com>
2024-07-11 19:24:35 +0900

    setting: cors

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         'http://localhost:3000',
+        'https://scam-coin.vercel.app'
     ],
     allow_credentials=True,
     allow_methods=["*"],
